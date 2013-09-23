4b74a9982ba10d2602b006284ed0ba716edefd8f
Joachim Lindborg <Joachim.Lindborg@lsys.se>
2013-09-23 18:48:20 +0200

    runIot script to start script

--- examples/runIoT.sh
+++ examples/runIoT.sh
@@ -3,4 +3,4 @@
 
 cd /home/pi/SleekXMPP/examples
 logger "starting IoT device server"
-python IoT_GPIODevice.py -j "sust9@jabber.sust.se" -p "QlpWkosust" -n "TestIoT" --debug 2>&1 | logger > /dev/null
+python IoT_GPIODevice.py -j "JID@yourdomain.com" -p "password" -n "TestIoT" --debug 2>&1 | logger > /dev/null
